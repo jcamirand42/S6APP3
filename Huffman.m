0c702baf28396269ee21f731b5aa73d46eefc5be
Tommy Savaria <savt2301@usherbrooke.ca>
2018-10-22 17:04:03 -0400

    Fini décodage Huffman

--- Huffman.m
+++ Huffman.m
@@ -1,49 +1,10 @@
-load('texte/Huffman_Test.mat');
+clc;
+close all;
+clear all;
 
-result = [];
-message = char(' ');
+load('texte/TexteEntrainement.mat');
+load('texte/TableAscii.mat');
+load('texte/Huffman_Test.mat')
 
-nbUn = 0;
-nbZero = 0;
-i = 1;
-
-% Détection de 8 uns consécutifs
-while nbUn < 8
-    if MessageHuffman_Test(i) == 1
-       nbUn = nbUn + 1;
-    else
-        nbUn = 0;
-    end
-    
-    i = i+1;
-end
-
-nbUn = 0;
-nbZero = 0;
-
-% Décodage du message
-while i <= length(MessageHuffman_Test)
-    if MessageHuffman_Test(i) == 0
-        nbZero = nbZero + 1;
-    else
-        nbUn = nbUn+1;
-        nbZero = 0;
-    end
-    
-    % Fin du message
-    if nbZero >= 1 && nbUn == 8
-        break;
-    end
-    
-    % Nouveau code recu
-    if nbZero >= 1 || nbUn == 256
-        code = dictionnary(nbUn + 1, 2);
-        [row, col] = find([CodeAscii{:}] == code, 1);
-        message = [message, CodeAscii{col, 2}];
-        result = [result nbUn];
-        nbUn = 0;
-    end
-    i = i+1;
-end
-
-message
+dict = genhuffmandict(TexteEntrainement, TableAscii);
+output = string2huffman(MessageHuffman_Test, dict)
