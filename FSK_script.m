07680d1647b06677ec3036fe278a198ae1cfdbba
jcamirand42 <johany.camirand@usherbrooke.ca>
2018-10-23 15:35:13 -0400

    FSK avec Huffman sans bruit

--- FSK_script.m
+++ FSK_script.m
@@ -7,3 +7,10 @@ load('texte/Huffman_Test.mat')
 fsk_input = [0.001:.001:5; MessageHuffman_Test]';
 
 sim('FSK');
+
+load('texte/TexteEntrainement.mat');
+load('texte/TableAscii.mat');
+load('texte/Huffman_Test.mat')
+
+dict = genhuffmandict(TexteEntrainement, TableAscii);
+output = huffman2string(fsk_output, dict)
