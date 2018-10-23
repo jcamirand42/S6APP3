e0fe9080351df822e11098cd9953796385611907
jcamirand42 <johany.camirand@usherbrooke.ca>
2018-10-23 15:40:29 -0400

    ASK avec huffman sans bruit

--- ASK_script.m
+++ ASK_script.m
@@ -1,7 +1,16 @@
-clc
-clear
-close all
+clc;
+clear;
+close all;
 
-load(strcat(pwd, '\donnees\MessageHuffman.mat'));
-plot(MessageHuffman_1);
-%sim('ASK')
+load('texte/Huffman_Test.mat')
+
+fsk_input = [0.001:.001:5; MessageHuffman_Test]';
+
+sim('ASK');
+
+load('texte/TexteEntrainement.mat');
+load('texte/TableAscii.mat');
+load('texte/Huffman_Test.mat')
+
+dict = genhuffmandict(TexteEntrainement, TableAscii);
+output = huffman2string(fsk_output, dict)
